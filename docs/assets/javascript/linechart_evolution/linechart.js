503061810ae1148dcc2761a1bda532de736a9b19
smaurizio87 <smaurizio87@protonmail.com>
2023-11-14 10:50:47 +0100

    Added yearly average temperatures

--- docs/assets/javascript/linechart_evolution/linechart.js
+++ docs/assets/javascript/linechart_evolution/linechart.js
@@ -3,21 +3,20 @@ d3.csv("https://raw.githubusercontent.com/thestippe/thestippe.github.io/main/dat
         d3.autoType).then(plotLinechart) // do not rely on default data types!
 
 function plotLinechart(data){
-        console.log(data.dt)
-        var maxval = 1.1*d3.max(data, d=> d.Italy) // find the appropriate scale, with some margin
+        var maxval = 1.1*d3.max(data, d=> d.Greece) // find the appropriate scale, with some margin
 
 
         // We first create an empty svg with the appropriate dimensions
         var svg = lineContainer.append("svg")
                 .attr("id", 'myid')
-                .attr("width", 800)
+                .attr("width", 1200)
                 .attr("height", 550)
 
 
 
         var x = d3.scaleTime()
                 .range([ 100, 800 ])
-                .domain([1978, 2021])
+                .domain([0, 1363])
 
         svg.append("g")
                 .call(d3.axisBottom(x)
@@ -31,17 +30,17 @@ function plotLinechart(data){
                 .call(d3.axisLeft(y)).attr("transform", "translate(100, 0)")
 
         svg.append("text")
-                .text("Gold price")
+                .text("Greece monthly temperature")
                 .attr("x", 400)
                 .attr("y", 20)
 
         svg.append("text")
-                .text("USD")
+                .text("C")
                 .attr("x", 0)
                 .attr("y", 200)
 
         svg.append("text")
-                .text("Year")
+                .text("index")
                 .attr("x", 450)
                 .attr("y", 540)
 
@@ -51,8 +50,8 @@ function plotLinechart(data){
       .attr("stroke", "steelblue")
       .attr("stroke-width", 3)
       .attr("d", d3.line()
-        .x(function(d) { return x(d.Date) })
-        .y(function(d) { return y(d.USD) }))
+        .x(function(d) { return x(d.id) })
+        .y(function(d) { return y(d.Greece) }))
         }
 
 
