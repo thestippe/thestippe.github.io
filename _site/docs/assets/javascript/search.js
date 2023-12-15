813c8243f9b96ea6166f5d31d0d6871ef24ba697
smaurizio87 <smaurizio87@protonmail.com>
2023-12-15 13:09:01 +0100

    Improved search function

--- _site/docs/assets/javascript/search.js
+++ _site/docs/assets/javascript/search.js
@@ -1,9 +1,22 @@
 
 function searchText(){
+        var event = window.event
+        console.log(event.keyCode)
+        if(event.keyCode == 13){
+                openFocus()
+        }
+        else if(event.keyCode == 40){
+                focusNext()
+        }
+        else if(event.keyCode == 38){
+                focusBefore()
+        }
+        else{
+        resetActive()
+
         closeList()
         data = document.getElementById('postList').querySelectorAll("li")
-        searchBar = document.getElementById('searchBar')
-        console.log(searchBar.value)
+        searchBar = document.getElementById('search_0')
 
         dtList = []
         for(elem of data){
@@ -14,20 +27,29 @@ function searchText(){
                 }
         }
         if(dtList.length>0 && searchBar.value.length>0){
-                suggestions = document.createElement('div')
-                out = ""
+                let suggestions = document.createElement('div')
+                ind = 1
                 for(elem of dtList){
-                        out += "<div class='searchResults'><a href='"+elem.tags.replace("_","-")+"'"+">"+elem.title+"</a></div>"
+                        // out += "<div id='search_"+ind+"' class='searchResults'><a href='"+elem.tags.replace("_","-")+"'"+">"+elem.title+"</a></div>"
+                        newDiv = document.createElement('div')
+                        newDiv.setAttribute('id', 'search_'+ind)
+                        newDiv.setAttribute('class', 'searchResults')
+                        href = document.createElement('a')
+                        href.innerHTML = elem.title
+                        href.setAttribute('href', elem.tags.replace('_', '-'))
+                        newDiv.appendChild(href)
+                        suggestions.appendChild(newDiv)
+                        ind += 1
 
                 }
 
-                suggestions.innerHTML = out
                 suggestions.setAttribute('id', 'suggestions')
 
                 suggestions.setAttribute('class', 'suggestionsResults')
                 searchBar.parentNode.appendChild(suggestions)
         }
         else{closeList()}
+}
 }
 
     function closeList() {
@@ -36,3 +58,73 @@ function searchText(){
             suggestions.parentNode.removeChild(suggestions);
     }
 
+function focusNext(){
+        let allResults =  document.querySelectorAll(".searchResults");
+        let resultNumber = allResults.length
+        const currInput = document.activeElement;
+        activeField = document.getElementById('search_focus')
+        let elemVal = +activeField.innerHTML
+        if(elemVal<resultNumber){
+                let nextId = +elemVal + 1
+                activeField.innerHTML = nextId
+                let currString = 'search_'+elemVal
+                let nextString = 'search_'+nextId
+
+                const nextInput = document.getElementById(nextString)
+                const currentInput = document.getElementById(currString)
+
+
+                nextInput.style.background = 'lightgray'
+                currentInput.style.background = '#fffcfa'
+
+                nextInput.classList.add('active')
+        }
+}
+
+function focusBefore(){
+        let allResults =  document.querySelectorAll(".searchResults");
+        let resultNumber = allResults.length
+        const currInput = document.activeElement;
+        activeField = document.getElementById('search_focus')
+        let elemVal = +activeField.innerHTML
+        if(elemVal>0){
+                let nextId = +elemVal - 1
+                activeField.innerHTML = nextId
+                let currString = 'search_'+elemVal
+                let nextString = 'search_'+nextId
+
+
+                const nextInput = document.getElementById(nextString)
+                const currentInput = document.getElementById(currString)
+
+                nextInput.classList.add('active')
+
+                if(nextId>0){
+                nextInput.style.background = 'lightgray'
+}
+                currentInput.style.background = '#fffcfa'
+
+        }
+}
+
+function openFocus(){
+
+        activeField = document.getElementById('search_focus')
+        if(+activeField.innerHTML>0){
+                console.log('opening')
+
+                let searchResult = document.getElementById('search_'+activeField.innerHTML)
+                window.open(searchResult.firstChild.href, '_self')
+        }else{
+                console.log('not opening')
+        }
+}
+
+function resetActive(){
+        let currentActive = document.getElementById('search_focus')
+        let activeElement = document.getElementById('search_'+currentActive.innerHTML) // restyle
+        activeElement.style.background = '#fffcfa'
+
+        currentActive.innerHTML = 0
+
+}
