2d55b962e5713c5113e4b4ff79543801f8f0d6bb
smaurizio87 <smaurizio87@protonmail.com>
2023-12-27 17:37:49 +0100

    Added delay effect to card appearence

--- _site/docs/assets/javascript/scrollEnter.js
+++ _site/docs/assets/javascript/scrollEnter.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 
   // get all of the elements with the 'card' class.
-  const scrollList = document.querySelectorAll(".card")
+  const scrollListEven = document.querySelectorAll(".cardEven")
 
   const callback = (entries, observer) => {
     entries.forEach((entry) => {
@@ -18,7 +18,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
   const myObserver = new IntersectionObserver(callback, options)
 
-  scrollList.forEach(scrollItem => {
+  scrollListEven.forEach(scrollItem => {
+    myObserver.observe(scrollItem)
+  })
+
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+  // get all of the elements with the 'card' class.
+  const scrollListOdd = document.querySelectorAll(".cardOdd")
+
+  const callback = (entries, observer) => {
+    entries.forEach((entry) => {
+
+      if (entry.isIntersecting) {
+
+        entry.target.classList.add("scrolled-in");
+
+      }
+      })
+  }
+  
+  const options = {}
+  
+  const myObserver = new IntersectionObserver(callback, options)
+
+  scrollListOdd.forEach(scrollItem => {
     myObserver.observe(scrollItem)
   })
 
